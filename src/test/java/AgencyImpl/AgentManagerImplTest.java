64943db82b65b2ac1c34e60bbe5d898c0b3238e5
Peter Navrátil <xnavrat8@fi.muni.cz>
2017-03-08 11:00:42 +0100

    added tests

---
+++ src/test/java/AgencyImpl/AgentManagerImplTest.java
@@ -0,0 +1,32 @@
+package AgencyImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pnavratil on 3/8/17.
+ */
+public class AgentManagerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void createAgent() throws Exception {
+
+    }
+
+    @Test
+    public void updateAgent() throws Exception {
+
+    }
+
+    @Test
+    public void removeAgent() throws Exception {
+
+    }
+
+}
---
+++ src/test/java/AgencyImpl/MissionManagerImplTest.java
@@ -0,0 +1,32 @@
+package AgencyImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pnavratil on 3/8/17.
+ */
+public class MissionManagerImplTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void createMission() throws Exception {
+
+    }
+
+    @Test
+    public void updateMission() throws Exception {
+
+    }
+
+    @Test
+    public void removeMission() throws Exception {
+
+    }
+
+}
